bb014c1b7861945bd0f16b9eedeb00117680c7f2
Raybabita <ray007oceans@gmail.com>
2021-12-25 13:31:55 +0000

    make connection and created GET BOOK APL

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+const { open } = require("sqlite");
+const path = require("path");
+const sqlite3 = require("sqlite3");
+
+const dbpath = path.join(__dirname, "goodreads.db");
+
+let db = null;
+
+const initializeDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqlite3.Database,
+    });
+
+    app.listen(3000, () => {
+      console.log("Server running at http://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB errer:${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDbAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+SELECT* 
+FROM book
+ORDER BY book_id`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
